57eea2caa2a62e82b3b47222482f859295275df9
Anton Sayutin <sayutosss@gmail.com>
2021-08-02 16:52:09 -0700

    add DB

--- app/src/main/java/com/example/newcovidapp/data/AllCovidInfo.kt
+++ app/src/main/java/com/example/newcovidapp/data/AllCovidInfo.kt
@@ -1,9 +1,15 @@
 package com.example.newcovidapp.data
 
-class AllCovidInfo(
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "allInfo")
+data class AllCovidInfo(
     val cases: Long,
     val todayCases: Long,
     val deaths: Long,
     val todayDeaths: Long,
     val recovered: Long,
-    val todayRecovered: Long)
+    val todayRecovered: Long,
+    @PrimaryKey
+    val id: Int = 0)
--- app/src/main/java/com/example/newcovidapp/ui/main/map_fragment/FragmentMap.kt
+++ app/src/main/java/com/example/newcovidapp/ui/main/map_fragment/FragmentMap.kt
@@ -5,11 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.room.Room
 import com.example.newcovidapp.R
+import com.example.newcovidapp.datasource.local.CovidDB
 
 class FragmentMap : Fragment(){
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_map, container, false)
+
+        val db = Room.databaseBuilder(requireContext(), CovidDB::class.java, "db").build()
+        val covidInfoDao = db.countriesInfoDao()
     }
 }
